460c232eed4792e567f5d992c83ba64be9894b1f
matthewfield <mail@mattfield.com>
2017-09-01 14:31:22 -0700

    Create unusual_time.js
    initial commit unsual_time.js

---
+++ units/time/unusual_time.js
@@ -0,0 +1,18 @@
+// unsual time units extension
+// Author: Matthew Field, mail@mattfield.com
+
+numi.addUnit({
+   "id": "atomus",
+   "phrases": "atomus",
+   "baseUnitId": "second",
+   "format" : "atomus",
+   "ratio" : .160
+});
+
+numi.addUnit({
+   "id": "blink",
+   "phrases": "blink, blinks",
+   "baseUnitId": "second",
+   "format" : "blinks",
+   "ratio" : .864
+});
