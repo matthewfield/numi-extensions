823282dcff8a2c88f3c95a9819c43438b1e92719
matthewfield <mail@mattfield.com>
2017-09-01 12:05:57 -0700

    Create grain.js
    Initial commit of grain.js

---
+++ units/weight/grain.js
@@ -0,0 +1,18 @@
+// grain extension
+// Author: Matthew Field, mail@mattfield.com
+
+numi.addUnit({
+   "id": "grain",
+   "phrases": "grain, gr",
+   "baseUnitId": "pound",
+   "format" : "grain",
+   "ratio" : 0.00014285714286
+});
+
+numi.addUnit({
+   "id": "grain",
+   "phrases": "grain, gr",
+   "baseUnitId": "gram",
+   "format" : "grain",
+   "ratio" : 0.0065
+});
