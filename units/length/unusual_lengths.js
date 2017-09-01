29c69976c3eedd4d3211738e2aa18afbb3aef33b
matthewfield <mail@mattfield.com>
2017-09-01 12:21:56 -0700

    Create unusual_lengths.js
    Initial commit unsual_lengths.js

---
+++ units/length/unusual_lengths.js
@@ -0,0 +1,34 @@
+// archaic/biblical/unusual lengths extension
+// Author: Matthew Field, mail@mattfield.com
+
+numi.addUnit({
+   "id": "cubit",
+   "phrases": "cubit, cubits",
+   "baseUnitId": "inch",
+   "format" : "cubit",
+   "ratio" : 18
+});
+
+numi.addUnit({
+   "id": "hand",
+   "phrases": "hand, hands",
+   "baseUnitId": "inch",
+   "format" : "hand",
+   "ratio" : 4
+});
+
+numi.addUnit({
+   "id": "horse",
+   "phrases": "horse, horses",
+   "baseUnitId": "m",
+   "format" : "horse",
+   "ratio" : 2.4385
+});
+
+numi.addUnit({
+   "id": "football_field",
+   "phrases": "football field, football fields",
+   "baseUnitId": "m",
+   "format" : "football fields",
+   "ratio" : 110
+});
