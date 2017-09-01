f21827270707d3c9d0e2d3ca444203cace14da27
matthewfield <mail@mattfield.com>
2017-09-01 14:27:23 -0700

    Create unsual_volumes.js
    initial commit

---
+++ units/volumes/unsual_volumes.js
@@ -0,0 +1,10 @@
+// unsual volume units extension
+// Author: Matthew Field, mail@mattfield.com
+
+numi.addUnit({ // a smidgen is 1/32 of a tsp or 1/2 a pinch
+   "id": "smidgen",
+   "phrases": "smidgen",
+   "baseUnitId": "m3",
+   "format" : "smidgen",
+   "ratio" : 0.0000001540287998
+});
