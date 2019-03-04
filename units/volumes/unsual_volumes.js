d14b9141b84d301f15bfb6bdabe4e48417a4279f
matthewfield <mail@mattfield.com>
2019-03-04 14:00:05 -0800

    Update unusual_volumes.js with pinch/dash/drop etc
    Bushel, pinch, dash, drop

--- units/volumes/unsual_volumes.js
+++ units/volumes/unsual_volumes.js
@@ -6,5 +6,37 @@ numi.addUnit({ // a smidgen is 1/32 of a tsp or 1/2 a pinch
    "phrases": "smidgen",
    "baseUnitId": "m3",
    "format" : "smidgen",
-   "ratio" : 0.0000001540287998
+   "ratio" : 0.00000011552159985
+});
+
+numi.addUnit({
+   "id": "bushel",
+   "phrases": "bushel",
+   "baseUnitId": "gallon",
+   "format" : "bushel",
+   "ratio" : 8
+});
+
+numi.addUnit({ // a dash is 1/64 of a fl oz
+    "id": "dash",
+   "phrases": "dash, dashes",
+   "baseUnitId": "m3",
+   "format" : "ds.",
+   "ratio" : 0.00000046208639941
+});
+
+numi.addUnit({ // a pinch is 1/128 of a fl oz
+    "id": "pinch",
+   "phrases": "pinch, pinches",
+   "baseUnitId": "m3",
+   "format" : "pn.",
+   "ratio" : 0.00000023104319971
+});
+
+numi.addUnit({ // a drop is 1/576 of a fl oz
+    "id": "drop",
+   "phrases": "drop, drops",
+   "baseUnitId": "m3",
+   "format" : "dr.",
+   "ratio" : 0.000000051342933268
 });
