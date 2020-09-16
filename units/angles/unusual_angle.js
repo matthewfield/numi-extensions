d527d5a384bdb1ea6b4b0403939a54996b52ec75
matthewfield <mail@mattfield.com>
2020-09-16 12:12:22 -0700

    Clarify mil is using NATO mils

--- units/angles/unusual_angle.js
+++ units/angles/unusual_angle.js
@@ -1,10 +1,11 @@
 // astronomical/archaic/biblical/unusual lengths extension
 // Author: Matthew Field, mail@mattfield.com
 
+//NATO mils (0.982 mrad)
 numi.addUnit({
    "id": "mil",
    "phrases": "mil, mils",
    "baseUnitId": "radian",
    "format" : "mil",
-   "ratio" : 1/1000
+   "ratio" : 0.9817477042/1000
 });
