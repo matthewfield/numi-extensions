c1ca755515e068a31f229183763adc94f46e8594
matthewfield <mail@mattfield.com>
2020-09-15 20:57:22 -0700

    Create unusual_angle.js

---
+++ units/angles/unusual_angle.js
@@ -0,0 +1,10 @@
+// astronomical/archaic/biblical/unusual lengths extension
+// Author: Matthew Field, mail@mattfield.com
+
+numi.addUnit({
+   "id": "mil",
+   "phrases": "mil, mils",
+   "baseUnitId": "radian",
+   "format" : "mil",
+   "ratio" : 1/1000
+});
