6eb0f1c210684e8619e72a490c1b2f0d825d0d0d
matthewfield <mail@mattfield.com>
2020-09-17 09:40:29 -0700

    add astronomical and imperial weights

---
+++ units/weight/astronomical.js
@@ -0,0 +1,26 @@
+// astronomical weights extension for numi.app
+// Author: Matthew Field, mail@mattfield.com
+
+numi.addUnit({
+   "id": "jupiter",
+   "phrases": "jupiter, jupiters",
+   "baseUnitId": "gram",
+   "format" : "jupiters",
+   "ratio" : 1.9E30
+});
+
+numi.addUnit({
+   "id": "sun",
+   "phrases": "sun, suns",
+   "baseUnitId": "gram",
+   "format" : "suns",
+   "ratio" : 2.0E33
+});
+
+numi.addUnit({
+   "id": "earth",
+   "phrases": "earth, earths",
+   "baseUnitId": "gram",
+   "format" : "earths",
+   "ratio" : 5.9736E27
+});
---
+++ units/weight/imperial_weights.js
@@ -0,0 +1,20 @@
+// imperial weights extension for numi.app
+// Author: Matthew Field, mail@mattfield.com
+
+numi.addUnit({
+   "id": "ton",
+   "phrases": "ton, tons",
+   "baseUnitId": "gram",
+   "format" : "ton",
+   "ratio" : 907184.7
+});
+
+numi.addUnit({
+   "id": "hundredweight",
+   "phrases": "cwt, hundredweight",
+   "baseUnitId": "gram",
+   "format" : "cwt",
+   "ratio" : 45359.24
+});
+
+
